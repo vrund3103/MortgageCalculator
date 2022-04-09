a5fa5d057f7aff6ccbc7827f61686945e44dd2c2
Vrund <vrundjoshi.vj@gmail.com>
2022-04-09 13:08:38 +0930

    changes to the main class for further improvisation

---
+++ Intro/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+  public static void main(String[] args) {
+   /* final byte monthsInYear = 12;
+    final byte percent = 100;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Principal: ");
+    int principal = scan.nextInt();
+
+    System.out.println("Annual Interest Rate: ");
+    float annualInterestRate = scan.nextFloat();
+    float monthlyInterest = annualInterestRate / percent / monthsInYear;
+
+    System.out.println("Period: ");
+    byte years = scan.nextByte();
+    int numberOfPayments = years * monthsInYear;
+
+    double mortgage = principal
+            * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+            / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+    System.out.println("Mortgage: " + mortgageFormatted); */
+
+    int x = 30;
+    int y = 30;
+    System.out.println(x==y);
+
+
+  }
+}
